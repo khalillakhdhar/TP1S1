714c9b86df011c8bb6569dd51301fadd0c044dab
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-11-30 10:37:12 +0100

    change

--- src/testes/Teste.java
+++ src/testes/Teste.java
@@ -10,7 +10,7 @@ public class Teste {
 		// TODO Auto-generated method stub
 Peripherique p1=new Peripherique("Lenovo", 4, "memory bus", 123456);
 Entree e1=new Entree("Toshiba",2, "clé usb",654321, 128);
-Sortie s1=new Sortie("Ecran",100, "HD 42pouce",122333, "LCD 4K");
+Sortie s1=new Sortie("Ecran",200, "HD 42pouce",122333, "LCD 4K");
 System.out.println(p1.toString());
 System.out.println(e1.toString());
 System.out.println(s1.toString());
